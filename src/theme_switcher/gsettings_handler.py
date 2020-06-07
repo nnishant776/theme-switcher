72b0326030385c54a2cb2c6e415ae64700e04f5d
Neeraj Nishant <nn54565852@gmail.com>
2020-06-08 02:18:10 +0530

    Changes:
        * Code cleanup
        * Generalization
        * Optimization based on use case

--- src/theme_switcher/gsettings_handler.py
+++ src/theme_switcher/gsettings_handler.py
@@ -3,13 +3,35 @@ from .themedb import Theme
 
 
 class GSettingsHandler(object):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if GSettingsHandler._instance is None:
+            GSettingsHandler._instance = super().__new__(cls)
+            GSettingsHandler._instance.desktop_schema = None
+            GSettingsHandler._instance.shell_schema = None
+        return GSettingsHandler._instance
 
     def __init__(self):
-        self._desktop_schema = Gio.Settings(schema='org.gnome.desktop.interface')
-        self._shell_schema = Gio.Settings(schema='org.gnome.shell.extensions.user-theme')
-        
-    def applyTheme(self, theme_obj: Theme):
-        self._desktop_schema.set_string('gtk-theme', theme_obj['gtk-theme'])
-        self._desktop_schema.set_string('icon-theme', theme_obj['icon-theme'])
-        self._desktop_schema.set_string('cursor-theme', theme_obj['cursor-theme'])
-        self._shell_schema.set_string('name', theme_obj['name'])
+        if GSettingsHandler._instance is None:
+            GSettingsHandler._instance = self
+        if self.desktop_schema is None:
+            self.desktop_schema = Gio.Settings(
+                schema='org.gnome.desktop.interface')
+        if self.shell_schema is None:
+            self.shell_schema = Gio.Settings(
+                schema='org.gnome.shell.extensions.user-theme')
+
+    @staticmethod
+    def getInstance():
+        if GSettingsHandler._instance is None:
+            GSettingsHandler()
+
+        return GSettingsHandler._instance
+
+    def applySchema(self, theme_obj: Theme):
+        self.desktop_schema.set_string('gtk-theme', theme_obj['gtk-theme'])
+        self.desktop_schema.set_string('icon-theme', theme_obj['icon-theme'])
+        self.desktop_schema.set_string(
+            'cursor-theme', theme_obj['cursor-theme'])
+        self.shell_schema.set_string('name', theme_obj['name'])
--- src/theme_switcher/theme_switcher.py
+++ src/theme_switcher/theme_switcher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import os
 import shutil
 import json
@@ -9,11 +9,14 @@ from pydbus import SessionBus
 
 from .theme_manager import ThemeManager, DBusNightLightManager
 
-share_path = "{}/../../../../share/theme-switcher/".format(os.path.dirname(__file__))
+share_path = "{}/../../../../share/theme-switcher/".format(
+    os.path.dirname(__file__))
+
 
 def get_system_theme() -> str:
     with open(share_path + 'config.json') as path_config_file:
-        theme_file = share_path + json.load(path_config_file)['filenames']['current_theme_file']
+        theme_file = share_path + \
+            json.load(path_config_file)['filenames']['current_theme_file']
 
     theme_name = 'Adwaita'
     with open(theme_file, 'r', encoding='utf-8') as theme_name_file:
@@ -21,71 +24,72 @@ def get_system_theme() -> str:
 
     return theme_name
 
+
 def initial_setup():
     # get path of current file
     here_path = os.path.dirname(__file__)
 
     # define the systemd service file to be used
     service_file_content = '\n'.join([
-    "[Unit]",
-    "Description = Automatic Theme Switcher\n",
+        "[Unit]",
+        "Description = Automatic Theme Switcher\n",
 
-    "[Install]",
-    "WantedBy = default.target\n",
-
-    "[Service]",
-    "Type = simple",
-    "ExecStart = {}/.local/bin/theme_switcher".format(os.getenv('HOME')),
-    "Restart = on-failure"
+        "[Service]",
+        "Type = simple",
+        "ExecStart = {}/../../../../bin/theme_switcher".format(
+            os.path.dirname(__file__)),
+        "Restart = on-failure"
     ])
-    
+
     # load the install config from the config.json file
     with open(share_path + 'config.json') as path_config_file:
         install_config = json.load(path_config_file)
         file_list = install_config['files']
         path_list = install_config['paths']
         file_name_list = install_config['filenames']
-        
+
         for key in file_list.keys():
             file_list[key] = file_list[key].format(here_path)
-            
+
         for key in path_list.keys():
             path_list[key] = path_list[key].format(here_path)
-        
+
     # get the status of initial setup
     with open(file_list['initial_setup_file'], 'r') as initial_setup_data:
         setup_done = initial_setup_data.read().strip()
-            
+
     if setup_done == '0':
-        
+
         try:
             # create directory structure for systemd service file and the desktop file
             os.mkdir(path_list['systemd_user_service_path'])
             os.mkdir(path_list['autostart_desktop_file_path'])
         except FileExistsError as e:
             print("File already exists")
-        
+
         # create the systemd service file with contents defined above
         with open(file_list['service_file'], 'w') as service_file_unit:
             service_file_unit.write(service_file_content)
 
         # copy the desktop file to its correct location
-        shutil.copyfile(share_path + file_name_list['desktop_file'], file_list['desktop_file'])
+        shutil.copyfile(
+            share_path + file_name_list['desktop_file'], file_list['desktop_file'])
 
         # remove the original file
         os.remove(share_path + file_name_list['desktop_file'])
-        
+
         # commit initial setup as done
         with open(file_list['initial_setup_file'], 'w') as initial_setup_data:
             initial_setup_data.write('1')
-        
+
+
 def main():
-    
+
     # do initial setup
     if len(sys.argv) >= 2 and sys.argv[1] == '--initial-setup':
         initial_setup()
         sys.exit(0)
-    
+
     # event loop object
     loop = GLib.MainLoop()
 
@@ -93,10 +97,10 @@ def main():
     session_bus = SessionBus()
 
     # obtain DBus NightLight manager service instance
-    DBusNightLightManager.create(session_bus)
-
+    dbus_nightlight_manager = DBusNightLightManager(session_bus)
+    
     # Set theme in the Theme Manager
-    ThemeManager.setThemeName(get_system_theme())
+    ThemeManager.getInstance().setThemeName(get_system_theme())
 
     # Apply theme according to the current night light status
     ThemeManager.applyTheme()
@@ -107,6 +111,7 @@ def main():
     except KeyboardInterrupt as e:
         loop.quit()
         print("\nExit by Control C")
+
 
 if __name__ == '__main__':
     main()
